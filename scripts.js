d286cce95e3d017eddf0748c352fd81b17123455
CageCell <143967084+Cagecellag@users.noreply.github.com>
2026-02-18 19:25:12 +0000

    second push

--- scripts.js
+++ scripts.js
@@ -1,3 +1,9 @@
-const bounceObserver = new IntersectionObserver(
+// Hide bounce observer
+const bounceObserver = new IntersectionObserver((entries) => {
+    if (entries[0].isIntersecting) {
 
-);
+    }
+});
+bounceObserver.observe(document.querySelector("#hideBounce"));
+
+// Image observer
